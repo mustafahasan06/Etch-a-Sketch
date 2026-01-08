93a9eef6393b962a2b850501cf01832c77d9aa96
mustafaHasan06 <mustafahasan06.tech@gmail.com>
2026-01-08 21:21:19 +0200

    make the createGrids function

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
-console.log("Hello world");
+let container = document.getElementById("container");
+let create = document.getElementById("create");
+const containerSize = 500;
+let userValue = 10;
+
+    createGrids(16);
+create.addEventListener('click', (e) => {
+    size = prompt("Enter a grid number:",10);
+    size = parseInt(userValue);
+    if (size > 0 && size <= 100) {
+        createGrids(size);
+    } else {
+        alert("Please enter a number between 1 and 100.");
+    }
+})
+function createGrids(squaresPerSide) {
+    container.innerHTML='';
+    const squareSize = containerSize / squaresPerSide;
+    const totalSquares = squaresPerSide * squaresPerSide;
+
+    for (let i = 0; i < totalSquares; i++) {
+        const divGrid = document.createElement("div");    
+        divGrid.style.width= `${squareSize}px`;
+        divGrid.style.height= `${squareSize}px`;
+
+        divGrid.classList.add("grid-item");
+        divGrid.addEventListener("mouseover", () =>{
+            divGrid.style.background="black";
+        });
+        
+        container.appendChild(divGrid);
+    }
+    
+       
+}
